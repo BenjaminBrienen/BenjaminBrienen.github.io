a4d045e44ddb8c32fe24945c9372f042d601fd5d
Benjamin Brienen <Benjamin.Brienen@outlook.com>
2024-09-29 23:55:22 +0200

    todo

---
+++ crates/resume/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("My resume in rust! :D");
+    println!("Feel free to look at my recent contributions to bevy.");
+    println!("I'm excited to start using rust day-to-day as a hobby now that we've settled into a comfortable and happy lifestyle here in the Netherlands.");
+    println!("I'm super excited about the opportunity to use it at for, for a living!");
+}
